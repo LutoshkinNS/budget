328758e29db20631e8f885f6882c2a82caff8608
Nikita Lutoshkin <n28280202@gmail.com>
2025-12-08 16:01:54 +0300

    client: Добавлен конфиг для key в eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
@@ -24,8 +25,10 @@ export default defineConfig([
     },
     plugins: {
       "simple-import-sort": simpleImportSort,
+      react,
     },
     rules: {
+      "react/jsx-key": "warn",
       "simple-import-sort/imports": [
         "error",
         {
