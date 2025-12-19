f32ec069d8099b7d162d18f229a80bcd9be3b99d
Nikita Lutoshkin <n28280202@gmail.com>
2025-12-19 12:08:10 +0300

    client: Добавлены костыль стили

--- frontend/src/app/layouts/private.tsx
+++ frontend/src/app/layouts/private.tsx
@@ -1,13 +1,20 @@
 import { Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
+const Space = () => {
+  return " ";
+};
+
 export function PrivateLayout() {
   return (
     <div>
       <div>
         <Link to="/login">Login</Link>
+        <Space />
         <Link to="/">Home</Link>
+        <Space />
         <Link to="/categories">Categories</Link>
+        <Space />
       </div>
       <hr />
       <Outlet />
