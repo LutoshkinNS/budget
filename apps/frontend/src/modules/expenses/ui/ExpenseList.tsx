7e7f87a410908f333e663be9b8d9bd8d50619c3d
Nikita Lutoshkin <n28280202@gmail.com>
2026-04-09 20:02:00 +0300

    client: Добавлено отображение категории

--- apps/frontend/src/modules/expenses/ui/ExpenseList.tsx
+++ apps/frontend/src/modules/expenses/ui/ExpenseList.tsx
@@ -1,9 +1,13 @@
 import { FormBlock } from "@/common/ui/form-block/FormBlock.tsx";
+import { useCategories } from "@/modules/categories";
 
 import { useExpenses } from "../useExpenses.ts";
 
 export function ExpenseList() {
   const { data } = useExpenses();
+  const { data: categories } = useCategories();
+
+  const categoryMap = new Map(categories.map((c) => [c.id, c.name]));
 
   return (
     <FormBlock legend={"Список трат"}>
@@ -11,6 +15,9 @@ export function ExpenseList() {
         {data.map((expense) => (
           <li key={expense.id}>
             {expense.amount}
+            {categoryMap.get(expense.categoryId)
+              ? ` [${categoryMap.get(expense.categoryId)}]`
+              : null}
             {expense.description ? ` - ${expense.description}` : null}
           </li>
         ))}
