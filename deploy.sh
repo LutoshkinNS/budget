cecb8afb442bcf5dcdf57c27ec9fa6dfd8d7369a
Nikita Lutoshkin <n28280202@gmail.com>
2026-06-09 22:39:08 +0300

    fix: build.sh

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,6 @@ git pull --ff-only origin master
 
 echo "==> [2/7] Pre-build cleanup"
 docker image prune -f
-docker builder prune -f
 
 echo "==> [3/7] Build server"
 docker compose --env-file .env build server
@@ -18,7 +17,6 @@ docker compose --env-file .env up -d --no-deps server
 
 echo "==> [5/7] Mid-build cleanup (free space before frontend)"
 docker image prune -f
-docker builder prune -f
 
 echo "==> [6/7] Build frontend"
 docker compose --env-file .env build frontend
@@ -28,7 +26,7 @@ docker compose --env-file .env up -d --no-deps frontend
 
 echo "==> Final cleanup"
 docker image prune -f
-docker builder prune -f
+docker builder prune -f --filter "until=24h"
 
 echo "==> Deploy complete"
 docker compose ps
